e740af4f0e425c8e3c36adf32ffa2a7c50a7e5f3
Josh Scanlan <75971516+joshuascan@users.noreply.github.com>
2023-04-03 11:50:53 -0700

    wraps app in auth0 provider

--- src/index.js
+++ src/index.js
@@ -2,10 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import { Auth0Provider } from "@auth0/auth0-react";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Auth0Provider
+      domain={process.env.AUTH0_DOMAIN}
+      clientId={process.env.AUTH0_CLIENT_ID}
+      authorizationParams={{
+        redirect_uri: `${window.location.origin}/magic-wallet`,
+      }}
+    >
+      <App />
+    </Auth0Provider>
   </React.StrictMode>
 );
